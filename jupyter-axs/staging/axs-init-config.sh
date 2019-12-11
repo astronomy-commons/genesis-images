757d1dd9baa719587c115c92fcb67e21b2b756a0
Steven Stetzler <steven.stetzler@gmail.com>
2019-12-11 07:48:49 +0000

    add jupyter-axs image

--- jupyter-axs/staging/axs-init-config.sh
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:0d62b69cb0361faf595628bf8e161f629be7cfbf024704c886ede881398ae158
-size 342
--- jupyter-axs/start-notebook.sh
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:031ff2a486675fa65c8f4f2d7a92717637780b3bf6c186916a8be14e4e0af4a0
-size 449
